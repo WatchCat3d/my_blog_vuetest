a7448f0a31a6dc2db7214b7412fb19240958e0e3
WatchCat <810549046@qq.com>
2018-02-27 23:17:42 +0800

    finish blog_content

--- src/components/Blog_content_page.vue
+++ src/components/Blog_content_page.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="blog_content">
-      <h1>标题</h1>
-      <h4>作者, 发表于时间</h4>
-      <p>经过十年的快速发展，高铁已成为人们日常出行的重要交通工具，“千里江陵一日还”早已变成现实，高铁改变的不仅是不断刷新的“中国速度”，更为区域与城市发展带来新的模式与机遇。
-
-高铁以高速、大容量、集约型、通勤化的特征，在中等距离的
-出行上具备极强的竞争力。高铁从无到有的十年也是中国城镇化快速发展的十年，无数大中小城市因高铁而串联，人、钱、物在城市间、地区间的流向更加便捷和高效，高铁网络正以前所未有的速度改变着中国城市的格局，借力高铁，一座座城市正在崛起。
-      </p>
+      <h1>{{blog.title}}</h1>
+      <h4>作者: {{blog.writer}} 发表于: {{blog.blog_date | standard_date}}</h4>
+      <p v-html="content"></p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Blog_content_page',
-  created: function () {
-      alert(document.URL);
+  data: function () {
+      return {
+          content: "",
+          blog: {}
+      }
+  },
+  created: function () {  //http://localhost:3000/#/blog_content_page/
+      var pattem = document.URL.replace(/http:\/\/localhost:3000\/#\/blog_content_page\//, "");
+      pattem = pattem.split('/');
+      var post = {
+          writer: pattem[0],
+          blog_date: pattem[1]
+      }
+      this.$http.post('./blog_content', post).then(function (res) {
+          this.blog = res.data;
+          this.content = res.data.content;
+          this.content = this.content.replace(/\n|\r\n/g,"<br/>");
+          this.content = this.content.replace(/ /g, "&nbsp;");
+      })
+  },
+  filters: {
+      standard_date: function (value) {
+          return (new Date(parseInt(value))).toLocaleDateString();
+      }
   }
 }
 </script>
--- src/components/Blog_list_page.vue
+++ src/components/Blog_list_page.vue
@@ -6,7 +6,7 @@
                     <h3 class="panel-title"><strong>文章标题 :{{item.title}}</strong></h3>
                 </div>
                 <div class="panel-body">
-                    作者: {{item.writer}} 日期: {{item.blog_date}}
+                    作者: {{item.writer}} 日期: {{item.blog_date | standard_date}}
                 </div>
             </router-link>
         </div>  
@@ -25,8 +25,13 @@ export default {
         var post = {};
         this.$http.post('./blog_list',post).then(function (res) {
             this.blog = res.data;
-            
+            this.blog = this.blog.reverse();
         })
+    },
+    filters: {
+        standard_date: function (value) {
+            return (new Date(parseInt(value))).toLocaleDateString();
+        }
     }
 }
 </script>
